881d0e616a9fa98ebf736954df56b1c02f475fb7
mirai-mirai <shiozaki.takashi@gmail.com>
2023-02-14 05:54:28 +0900

    aaa

---
+++ src/.vitepress/config.js
@@ -0,0 +1,35 @@
+export default {
+ title: '画像解析PoC',
+ // description: 'タクミアイズツール集',
+ themeConfig: {
+  siteTitle: 'ツール集',
+  sidebar: [
+   {
+    text: '画像解析',
+    items: [
+     { text: '画像切り出し', link: '/item-a' },
+     { text: '学習(TeachableMachine)', link: 'https://teachablemachine.withgoogle.com/train/tiny_image' },
+     { text: '推論', link: '/item-b' },
+    ]
+   },
+   {
+    text: '映像再生',
+    items: [
+     { text: 'Item C', link: '/item-c' },
+    ]
+   }
+  ],
+  nav: [
+   // { text: '画像切り抜き', link: '/imageTrimmer' },
+   // {
+   //  text: 'Dropdown Menu',
+   //  items: [
+   //   { text: 'Item A', link: '/item-1' },
+   //  ]
+   // }
+  ],
+ },
+ srcDir: './src',
+ outDir: './doc',
+ lastUpdated: true,
+}
